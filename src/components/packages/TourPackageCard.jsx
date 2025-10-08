1112136a1233454cfe5cfa8f62f6115ffd6530a6
Shantanu Chavhan <shantanuchavhan10@gmail.com>
2025-10-08 16:20:45 +0530

    commit

--- src/components/packages/TourPackageCard.jsx
+++ src/components/packages/TourPackageCard.jsx
@@ -27,14 +27,14 @@ const TourPackageCard = ({ pkg }) => (
         <Link to={`/package/${pkg.id}`}>
           <h3 className="text-2xl font-bold text-gray-800 mb-2 hover:text-emerald-600 transition duration-300">{pkg.name}</h3>
         </Link>
-        <div className="flex items-center text-sm text-gray-500">
-          <span className="flex items-center mr-1 md:mr-4">
+        <div className="flex items-center text-sm text-gray-500 w-full">
+          <span className="flex items-center mr-1 md:mr-4 text-[12px]">
             <svg className="w-4 h-4 mr-1 text-emerald-500" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
             </svg>
             {pkg.destination}
           </span>
-          <span className="flex items-center">
+          <span className="flex items-center text-[12px] w-[80%]">
             <svg className="w-4 h-4 mr-1 text-emerald-500" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
             </svg>
