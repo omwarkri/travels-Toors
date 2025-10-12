a594ac5c19ed430babe6322c7c0de3c90b7bd65a
Shantanu Chavhan <shantanuchavhan10@gmail.com>
2025-10-13 01:02:46 +0530

    commit

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -122,7 +122,7 @@ const Navigation = () => {
           >
             <img
               src="https://res.cloudinary.com/dl2gcscfa/image/upload/v1759578583/logo_dps0pd.jpg"
-              className="w-20 sm:w-28 h-auto ml-2"
+              className="w-28 sm:w-28 h-auto ml-2"
               alt="Logo"
             />
           </Link>
