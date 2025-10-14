2d19d03b53fd1455f595216ff1c188365925d9a4
Shantanu Chavhan <shantanuchavhan10@gmail.com>
2025-10-14 18:41:00 +0530

    commit

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/common/Footer";
 
 const ContactPage = () => {
   return (
-    <div id="contact" className="pt-16 min-h-screen bg-gray-50">
+    <div id="contact" className="pt-0 min-h-screen bg-gray-50">
       {/* Main Content */}
       <section className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         {/* Header */}
